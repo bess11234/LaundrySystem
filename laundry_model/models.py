88a603166ac3646a95077ab044a6965831622c1c
kasomsri <bess11234@hotmail.com>
2024-09-19 00:02:24 +0700

    VALIDATE phone field

--- laundry_model/models.py
+++ laundry_model/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-from phonenumber_field.modelfields import PhoneNumberField
-
 
 # Create your models here.
 class CustomUserManager(BaseUserManager):
