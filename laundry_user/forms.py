a292765b646be8a9173a932dcb57264100c1cd3c
kasomsri <bess11234@hotmail.com>
2024-09-19 00:01:35 +0700

    VALIDATE phone field

--- laundry_user/forms.py
+++ laundry_user/forms.py
@@ -3,9 +3,19 @@ from django import forms
 
 from laundry_model.models import Users
 
+from django.core.exceptions import ValidationError
+
 class RegisterForm(UserCreationForm):
     class Meta:
         model = Users
         fields = ["email", "first_name", "last_name", "phone"]
     
     phone = forms.CharField(max_length=10, min_length=10)
+    
+    def clean_phone(self):
+        phone = self.cleaned_data.get("phone")
+        if phone[2] not in ("02","06","08","09"):
+            raise ValidationError(
+                "Phone should start with '02', '06', '08', '09'"
+            )
+        return phone
